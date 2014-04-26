d33cbe9c2bc55368845bcabb425339708ae4cdfa
Mark Bennett <mwbennett@wesleyan.edu>
2014-04-26 11:25:29 -0400

    layout styles and login form

--- blogging_app.rb
+++ blogging_app.rb
@@ -11,6 +11,24 @@ set :database, "sqlite3:///blog.sqlite3"
 
 # Routes 
 # 
+get '/about' do 
+	erb :about
+end 
+
+get '/create' do 
+	erb :create
+end 
+
 get '/' do 
 	erb :home
 end 
+
+get '/login' do 
+	erb :log_in
+end 
+
+get '/signup' do 
+	erb :sign_up
+end 
+
+
